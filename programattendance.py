638b8c410e0484041ce02f3837c31c6f1219de73
Prakriti Gupta <102422783+prakriti0903@users.noreply.github.com>
2022-10-26 20:04:32 +0530

    issue is solved

---
+++ programattendance.py
@@ -0,0 +1,12 @@
+num_of_class=100
+num_of_class_attend=input("please enter your attendance")
+per=(num_of_class_attend/num_of_class)*100
+if per<75:
+  print("not allowed")
+  con=input("if medical certificate press yes otherwise no")
+  if con=="yes":
+    print("allowed)
+          else:
+          print("not allowed")
+          else:
+          print("allowed")
